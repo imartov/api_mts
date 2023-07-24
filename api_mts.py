551bd45b9f8db526755269b73d525e67b901a36a
Kozyrev Aleksandr S <alexandr.kosyrew@mail.ru>
2023-07-24 23:20:40 +0300

    add logger

--- api_mts.py
+++ api_mts.py
@@ -1,102 +1,96 @@
 import os, json
 import requests
 from dotenv import load_dotenv
+from loguru import logger
 
 
+# add logger
+logger.add('debug.log', format='{time} {level} {message}', level='DEBUG', rotation='10 KB', compression='zip')
+
+@logger.catch()
 class ApiMTS:
     ''' Class for sending messages and getting reports '''
-
+    
     def __init__(self) -> None:
-        self.job_id = None
-        self.message_id = None
-        self.extra_id = None
-
+        
         # get environment variables
         load_dotenv()
         self.CLIENT_ID = os.getenv("CLIENT_ID")
         self.LOGIN = os.getenv("LOGIN")
         self.PASSWORD = os.getenv("PASSWORD")
 
-        # api urls for send messages
-        self.urls_send_messages = {
-            "mass_broadcast_sync": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/broadcast/sync",
-            "mass_broadcast": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/broadcast",
-            "mass_batch_sync": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/batch/sync",
-            "mass_batch": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/batch",
-            "one_message": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/simple"
+
+    def get_url(self, by:str, send_message=False, get_report=False, job_id=None, message_id=None, extra_id=None) -> str:
+        ''' method for getting url '''
+        urls_send_messages = {
+                "mass_broadcast_sync": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/broadcast/sync",
+                "mass_broadcast": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/broadcast",
+                "mass_batch_sync": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/batch/sync",
+                "mass_batch": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/batch",
+                "one_message": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/simple"
+            }
+        urls_get_reports = {
+            "job_id": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/job/status/{job_id}",
+            "message_id_advanced": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/{message_id}/advanced",
+            "message_id_simple": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/{message_id}/simple",
+            "extra_id_advanced": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/external/{extra_id}/advanced",
+            "extra_id_simple": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/external/{extra_id}/simple"
         }
+        if send_message:
+            return urls_send_messages[by]
+        elif get_report:
+            return urls_get_reports[by]
+
+
+    def send_message(self, by:str, request_params:dict) -> dict:
+        ''' method for send messages as one as mass '''
         
-        # api urls for get reports
-        self.urls_get_reports = {
-            "by_job_id": f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/job/status/{self.job_id}",
-            "by_message_id_advanced": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/{self.message_id}/advanced",
-            "by_message_id_simple": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/{self.message_id}/simple",
-            "by_extra_id_advanced": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/external/{self.extra_id}/advanced",
-            "by_extra_id_simple": f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/external/{self.extra_id}/simple"
-        }
-
-
-    def send_message(self, url:str, request_params:dict) -> dict:
-        ''' function for send messages as one as mass '''
+        url = self.get_url(send_message=True, by=by)
         response = requests.post(url=url, json=request_params, auth=(self.LOGIN, self.PASSWORD))
-        return {"http_code": int(response.status_code), "resp_json": response.json()}
+        return {"http_code": int(response.status_code), "response_json": response.json()}
     
 
-    def get_report(self, url:str, ) -> dict:
-        ''' function for get reports '''
+    def get_report(self, by:str, job_id=None, message_id=None, extra_id=None) -> dict:
+        ''' method for get reports '''
+        
+        url = self.get_url(get_report=True, by=by, job_id=job_id, message_id=message_id, extra_id=extra_id)
         response = requests.get(url=url, auth=(self.LOGIN, self.PASSWORD))
+        return {"http_code": int(response.status_code), "response_json": response.json()}
+    
 
+    def send_broadcast_mass_messages_and_get_report_by_job_id(self, request_params:dict) -> None:
+        ''' the popular request method'''
+        message = self.send_message(by="mass_broadcast", request_params=request_params)
+        if message["http_code"] == 200:
+            message_resp_json = message["response_json"]
+            job_id = message_resp_json["job_id"]
+            report = self.get_report(by="job_id", job_id=job_id)
+            return {"resp_message": message_resp_json, "resp_report": report["response_json"]}
+        else:
+            print("\nSomething is going wrong: ")
+            print(f"http-code of sending message: {message['http_code']}")
+            # TODO: return
 
-
-
-
-    def send_messages_broadcast_sync(self, request_params:dict) -> dict:
-        ''' this method is for mass-sending and returns detail response '''
-        url = f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/broadcast/sync"
-
-        # sending sms-messages
-        resp = requests.post(url=url, json=request_params, auth=(self.LOGIN, self.PASSWORD))
-        return {"http_code": int(resp.status_code), "resp_json": resp.json()}
-
-
-    def send_messages_broadcast(self, request_params:dict) -> dict:
-        ''' this method is for mass-sending and returns short response - job_id '''
-        url = f"https://api.communicator.mts.by/{self.CLIENT_ID}/json2/broadcast"
-
-        # sending sms-messages
-        resp = requests.post(url=url, json=request_params, auth=(self.LOGIN, self.PASSWORD))
-        return {"http_code": int(resp.status_code), "resp_json": resp.json()}
-
-
-    def get_report_by_message_id(self, path_to_file:str) -> None:
-        ''' get detail report by message_id'''
-        with open(path_to_file, "r", encoding="utf-8") as file:
-            data = json.load(file)
-
-        reports_list = []
-        for message_delivery in data:
-            for message in message_delivery["messages"]:
-                message_id = message["message_id"]
-
-                url = f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/{message_id}/advanced"
-                resp = requests.get(url=url, auth=(self.LOGIN, self.PASSWORD))
-                reports_list.append(resp.json())
-                print("\nReport request: ")
-                print("1. HTTP code of report request: ", resp.status_code)
-                print("2. Return data: ", resp.json())
-        return reports_list
-
-
-    # def get_report_by_message_id(self, message_id:str) -> None:
-    #     ''' get detail report '''
-    #     url = f"https://api.communicator.mts.by/{self.CLIENT_ID}/dr/{message_id}/advanced"
-    #     resp = requests.get(url=url, auth=(self.LOGIN, self.PASSWORD))
-    #     print("\nReport request: ")
-    #     print("1. HTTP code of report request: ", resp.status_code)
-    #     print("2. Return data: ", resp.json())
-    #     print(resp)
+    
+    def send_broadcast_sync_mass_messages_and_get_report_by_message_id(self, request_params:dict) -> dict:
+        ''' another popular request method '''
+        message = self.send_message(by="mass_broadcast_sync", request_params=request_params)
+        if message["http_code"] == 200:
+            message_resp_json = message["response_json"]
+            message_id_list = []
+            for message in message_resp_json["messages"]:
+                message_id_list.append(message["message_id"])
+            report_list = []
+            for message_id in message_id_list:
+                report = self.get_report(by="message_id", message_id=message_id)
+                report_list.append(report["response_json"])
+            return {"resp_message": message_resp_json, "resp_report": report_list}
+        else:
+            print("\nSomething is going wrong: ")
+            print(f"http-code of sending message: {message['http_code']}")
+            # TODO: return
 
 
 if __name__ == "__main__":
     p = ApiMTS()
-    p.get_report_by_message_id(path_to_file="sent_messages\\response_data\\23_07_2023.json")
+    p.get_report(by="message_id_advanced", message_id=123456789)
