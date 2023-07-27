bb14d0aa5b06f9a00a39dcb58c5e965bbdc87a5c
Kozyrev Aleksandr S <alexandr.kosyrew@mail.ru>
2023-07-27 12:21:30 +0300

    finished develop get_url method in api_mts.py

--- api_mts.py
+++ api_mts.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 from loguru import logger
 # from utils import notice_exception
+# TODO: fix circular import
 
 
 # add logger
@@ -23,15 +24,14 @@ class ApiMTS:
     def get_url(self, by:str, job_id=None, message_id=None, extra_id=None) -> str:
         ''' method for get url '''
         load_dotenv()
-        urls_get_reports = {
-            "job_id": os.getenv("GR_JOB_ID").format(client_id=self.CLIENT_ID, job_id=job_id),
-            "message_id_advanced": os.getenv("GR_MESSAGE_ID_ADVANCED").format(client_id=self.CLIENT_ID, message_id=message_id),
-            "message_id_simple": os.getenv("GR_MESSAGE_ID_SIMPLE").format(client_id=self.CLIENT_ID, message_id=message_id),
-            "extra_id_advanced": os.getenv("GR_EXTRA_ID_ADVANCED").format(client_id=self.CLIENT_ID, extra_id=extra_id),
-            "extra_id_simple": os.getenv("GR_EXTRA_ID_SIMPLE").format(client_id=self.CLIENT_ID, extra_id=extra_id)
-        }
-        return os.getenv(by.upper()).format(client_id=self.CLIENT_ID)
-        # TODO: get active params of method, maybe __namemethod__
+        if job_id:
+            return os.getenv(by.upper()).format(client_id=self.CLIENT_ID, job_id=job_id)
+        elif message_id:
+            return os.getenv(by.upper()).format(client_id=self.CLIENT_ID, message_id=message_id)
+        elif extra_id:
+            return os.getenv(by.upper()).format(client_id=self.CLIENT_ID, extra_id=extra_id)
+        else:
+            return os.getenv(by.upper()).format(client_id=self.CLIENT_ID)
 
 
     @logger.catch()
@@ -39,8 +39,12 @@ class ApiMTS:
         ''' method for send messages as one as mass '''
         url = self.get_url(send_message=True, by=by)
         response = requests.post(url=url, json=request_params, auth=(self.LOGIN, self.PASSWORD))
-        print(f"\nresp_json: {response.json()}")
-        return {"http_code": int(response.status_code), "response_json": response.json()}
+        try:
+            return {"http_code": int(response.status_code), "response_json": response.json()}
+        except Exception as text_exception:
+            notice_exception(text_exception=text_exception)
+            # TODO: email
+            return {"http_code": int(response.status_code)}
     
 
     @logger.catch()
@@ -49,9 +53,12 @@ class ApiMTS:
         url = self.get_url(get_report=True, by=by, job_id=job_id, message_id=message_id, extra_id=extra_id)
         right_resp = False
         seconds = 0
+        limit_seconds = 180
         while not right_resp:
-            if seconds >= 120:
-                # TODO: notice 
+            if seconds >= limit_seconds:
+                text_exception = f"Количество секунд ожидания ответа для получения отчета превысило {limit_seconds}"
+                notice_exception(text_exception=text_exception)
+                # TODO: email
                 break
             else:      
                 response = requests.get(url=url, auth=(self.LOGIN, self.PASSWORD))
@@ -60,20 +67,23 @@ class ApiMTS:
                 else:
                     seconds += 2
                     time.sleep(2)
-        return {"http_code": int(response.status_code), "response_json": response.json()}
+        try:            
+            return {"http_code": int(response.status_code), "response_json": response.json()}
+        except Exception as text_exception:
+            notice_exception(text_exception=text_exception)
     
 
     def send_broadcast_mass_messages_and_get_report_by_job_id(self, request_params:dict) -> dict:
         ''' the popular request method for sennding mass messages using
          by broadcast and getting report by job_id for full company '''
+        message = self.send_message(by="mass_broadcast", request_params=request_params)
         try:
-            message = self.send_message(by="mass_broadcast", request_params=request_params)
             message_resp_json = message["response_json"]
             job_id = message_resp_json["job_id"].strip()
             report = self.get_report(by="job_id", job_id=job_id)
-            print("\nresp_message", message_resp_json)
-            print("resp_report", report["response_json"])
-            return {"resp_message": message_resp_json, "resp_report": report["response_json"]}
+            return {"resp_message": message_resp_json,
+                    "http_code": message["http_code"],
+                    "resp_report": report["response_json"]}
         except Exception as text_exception:
             notice_exception(text_exception=text_exception)
             # TODO: message to email
@@ -84,8 +94,8 @@ class ApiMTS:
     def send_broadcast_sync_mass_messages_and_get_report_by_message_id(self, request_params:dict) -> dict:
         ''' another popular request method for sending mass messages
          using by braodcast and get report for ever message by message_id '''
+        message = self.send_message(by="mass_broadcast_sync", request_params=request_params)
         try:
-            message = self.send_message(by="mass_broadcast_sync", request_params=request_params)
             message_resp_json = message["response_json"]
             message_id_list = []
             for message in message_resp_json["messages"]:
@@ -117,10 +127,9 @@ class ApiMTS:
             notice_exception(text_exception=text_exception)
             return {"resp_message": message_resp_json,
                     "http_code": message["http_code"]}
-            # TODO: try message with viber with image and links, why vuber is not allowed?
             # TODO: save to data http-code
 
 
 if __name__ == "__main__":
-    p = ApiMTS().get_url(by="SM_MASS_BROADCAST_SYNC")
+    p = ApiMTS().get_url(by="GR_EXTRA_ID_ADVANCED", extra_id="12345")
     print(p)
